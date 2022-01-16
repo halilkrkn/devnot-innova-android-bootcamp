1b5d38cd389bae2302388e1255b71c6b71f31fa8
Halilkrkn <halilkrkn000@gmail.com>
2022-01-16 13:04:46 +0300

    Navigation ile Sayfalar Arası Geçiş ve  Veri Gönderimi Yapıldı
    - Navigation Componentleri ile;
    - İlk olarak sayfalar arası geçişi yaptırdık.
    - Aynı intent gibi verileri yani dataları başka bir sayfaya gönderdik.
    - Gönderilen sayfadan verileri aldık.

--- NavigationKullanimi/app/src/main/java/com/example/navigationkullanimi/AnasayfaFragment.kt
+++ NavigationKullanimi/app/src/main/java/com/example/navigationkullanimi/AnasayfaFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.navigationkullanimi.databinding.FragmentAnasayfaBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -20,7 +21,13 @@ class AnasayfaFragment: Fragment() {
 
 
         binding.buttonBasla.setOnClickListener{
-            Snackbar.make(it, "Merhaba", Snackbar.LENGTH_LONG).show()
+//            Snackbar.make(it, "Merhaba", Snackbar.LENGTH_LONG).show()
+            // Navigatiom İşlemleri işlemler ile sayfalar arası veri gönderme
+            // Burada AnasayfaFragmentten OyunEkraniFargmente verileri göndermek için o AnasayfaFragmentDirections sayesinde OyunEkraniFragmente verileri gönderdik.
+            // geciş değişkenini navigate fonk. içerisine koyarak OyunEkraniFargmente verileri gönderdik ve sayfayı yönlendirdik.
+            val kisi = Kisiler("Mehmet", 34, 1.98f, false)
+            val gecis = AnasayfaFragmentDirections.oyunEkraninaGecis("Ahmet",25,1.74f,true, kisi)
+            Navigation.findNavController(it).navigate(gecis)
         }
         return binding.root
     }
---
+++ NavigationKullanimi/app/src/main/java/com/example/navigationkullanimi/Kisiler.kt
@@ -0,0 +1,10 @@
+package com.example.navigationkullanimi
+
+import java.io.Serializable
+
+data class Kisiler(
+    val ad:String,
+    val yas: Int,
+    val boy: Float,
+    val bekar: Boolean
+): Serializable
