437bd4dc5ebcccb1081fcff84fefd5426717d33a
Halilkrkn <halilkrkn000@gmail.com>
2022-01-20 16:40:14 +0300

    Linkedin UI Clone Tasarımı Yapıldı.

---
+++ Odevler/Odev3/LinkedinUIClone/app/src/main/java/com/example/linkedinuiclone/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.linkedinuiclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.linkedinuiclone.R
+import com.example.linkedinuiclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Bottom Navigation Kodlaması
+        // Oluşturmuş olduğumuz bottom_nav_graph özelliği oluşturmş olduğumuz fragmentlerle kullanabilmek için navHostFragment e aktarıyoruz.
+        val nav_hos_fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //        // activity_main içerisinde oluşturduğumuz bottom navigation view bileşeninin kurulumunu yaptık ve sayfalar arası geçişleri sağlaması içinde navControllerı veriyoruz.
+        NavigationUI.setupWithNavController(binding.bottomNav,nav_hos_fragment.navController)
+    }
+
+
+}
---
+++ Odevler/Odev3/LinkedinUIClone/app/src/main/java/com/example/linkedinuiclone/ui/fragments/LinkedinMainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.linkedinuiclone.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.linkedinuiclone.R
+import com.example.linkedinuiclone.databinding.FragmentLinkedinMainBinding
+import com.example.linkedinuiclone.ui.adapter.LinkedinMainAdapters
+
+class LinkedinMainFragment : Fragment() {
+    private lateinit var binding: FragmentLinkedinMainBinding
+    lateinit var linkedinMainAdapter: LinkedinMainAdapters
+    private val itemsList = ArrayList<String>()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLinkedinMainBinding.inflate(inflater, container, false)
+
+        linkedinMainAdapter = LinkedinMainAdapters(itemsList)
+        binding.apply {
+            linkedinMainRecyclerView.setHasFixedSize(true)
+            linkedinMainRecyclerView.adapter = linkedinMainAdapter
+        }
+        prepareItems()
+        return binding.root
+    }
+    private fun prepareItems() {
+        itemsList.add("Item 1")
+        itemsList.add("Item 2")
+        linkedinMainAdapter.notifyDataSetChanged()
+    }
+}
