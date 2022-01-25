d2f6842435d8aede25f67c3fdf6d91055614a868
Halilkrkn <halilkrkn000@gmail.com>
2022-01-25 16:26:10 +0300

    Toast, SnackBar, AlertDialog Yapısı Öğrenildi.

--- KullaniciEtkilesimi/app/src/main/java/com/example/kullanicietkilesimi/MainActivity.kt
+++ KullaniciEtkilesimi/app/src/main/java/com/example/kullanicietkilesimi/MainActivity.kt
@@ -16,40 +16,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.apply {
+        binding.apply
+        buttonToast.setOnClickListener {
+            Toast.makeText(applicationContext, "Merhaba", Toast.LENGTH_SHORT).show()
+        }
 
-            buttonToast.setOnClickListener{
-                Toast.makeText(applicationContext,"Merhaba",Toast.LENGTH_SHORT).show()
-            }
-
-            buttonSnackBar.setOnClickListener{
-               val sb = Snackbar.make(it,"İnternet Bağlantısı Yok!",Snackbar.LENGTH_LONG)
-                sb.setAction("TEKRAR DENE"){
+        buttonSnackBar.setOnClickListener {
+            val sb = Snackbar.make(it, "İnternet Bağlantısı Yok!", Snackbar.LENGTH_LONG)
+            sb.setAction("TEKRAR DENE") {
 //                        Snackbar.make(it,"Silindi",Snackbar.LENGTH_LONG).show()
-                    }
-                sb.setActionTextColor(Color.RED) // Action veya  Button Rengi
-                sb.setTextColor(Color.BLUE) // Mesaj Rengi
-                sb.setBackgroundTint(Color.WHITE) // Arkaplan Rengi
-                sb.show()
+            }
+            sb.setActionTextColor(Color.RED) // Action veya  Button Rengi
+            sb.setTextColor(Color.BLUE) // Mesaj Rengi
+            sb.setBackgroundTint(Color.WHITE) // Arkaplan Rengi
+            sb.show()
+        }
+
+        buttonAlert.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this@MainActivity)
+            alertDialog.setTitle("Başlik")
+            alertDialog.setMessage("İçerik")
+
+            alertDialog.setPositiveButton("Tamam") { text, listener ->
+                Log.e("Alert", "Tamam Seçildi.")
             }
 
-            buttonAlert.setOnClickListener{
-                val alertDialog = AlertDialog.Builder(this@MainActivity)
-                alertDialog.setTitle("Başlik")
-                alertDialog.setMessage("İçerik")
-
-                alertDialog.setPositiveButton("Tamam"){ text,listener ->
-                    Log.e("Alert", "Tamam Seçildi.")
-                }
-
-                alertDialog.setNegativeButton("İptal Et"){ text,listener ->
-                    Log.e("Alert", "İptal Seçildi.")
-                }
-
-                alertDialog.create().show()
+            alertDialog.setNegativeButton("İptal Et") { text, listener ->
+                Log.e("Alert", "İptal Seçildi.")
             }
-
+            alertDialog.create().show()
         }
 
     }
+
+}
 }
